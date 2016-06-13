939f525fa8fb2492be9b16f8e5e9b8ff9af5db50
JZhang <jzhangnu@gmail.com>
2016-06-13 17:51:41 -0500

    react videoSearch app demo

--- React/ReactRedux/src/components/search_bar.js
+++ React/ReactRedux/src/components/search_bar.js
@@ -14,25 +14,37 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = { term:'' };
+    this.state = { term:'Starting Value' };
   }
-//every time the state of the component changed, the render function will be rerendered
+//every time the state of the component changed
+//the render function will be rerendered
 //functional component don't have state. Only class components do
 
   render() {
 //still a function
-//every class have the render function must return some jsx otherwise it will end up with a error
+//every class have the render function must return some jsx
+//otherwise it will end up with a error
+//do not change the value just change trigger the events
+//event.target.value
     return (
-      <div>
-        <input onChange = {(event) => this.setState({ term: event.target.value })} />
-        //do not change the value just change trigger the events
+      <div className = 'search-bar'>
+        <input
+          value = {this.state.term}
+          onChange = { event => this.onInputChange(event.target.value)} />
         <p>The value of term is: {this.state.term}</p>
       </div>
     );
     //emit a change event
   }
-}
 
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+
+  }
+
+}
 
 
 
--- React/ReactRedux/src/index.js
+++ React/ReactRedux/src/index.js
@@ -5,12 +5,14 @@
 //React diversify to two parts
 //core part: know how to work with react component(how to render them, how to nest them together)
 
-import React from 'react';
+import React, {Component} from 'react';
 
 //Another part take the component's generated HTML and insert/put into the Dom called ReactDom
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 //youtube api key
 //npm install --save youtube-api-search
 //save to the package.json
@@ -20,10 +22,48 @@ const api_key = 'AIzaSyBMjiShZO2nfufzgHsIdCuUy_Gjubzypa8';
 //=> arrow function
 //are a short syntax for function expressions
 //can use them in place of expressions like function (foo) {...}
-const App = () => {
-  return <div>hello world</div>;
-}
+class App extends Component{
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      videos:[],
+      selectedVideo: null
+    };
+
+    this.videoSearch('nba')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: api_key, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange ={ term => this.videoSearch(term) }/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos= {this.state.videos}/>
+      </div>
+    );
+  }
+}
+/*
+const App = () => {
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
+}
+*/
+//whenever we want to use state, we must have a class based component
 //pass instance to the app
 ReactDOM.render(<App />, document.querySelector('.container'));
-ReactDOM.render(<SearchBar />, document.querySelector('.container'));
